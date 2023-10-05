5e4e044001ae07d9659b2677d297098b7c39f893
B00156808 <b00156808@mytudublin.ie>
2023-10-05 15:29:08 +0100

    ch2

---
+++ Challenge2_b00156808/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    //adding cooldown 
+    private float cool = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog while cooldown is less than or equal to 0
+        if (Input.GetKeyDown(KeyCode.Space) && cool <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //cooldown value is 1 when dog spawned
+            cool = 1;
+        }
+        //while cooldown is above 0
+        cool -= Time.deltaTime;
+    }
+}
---
+++ Challenge2_b00156808/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnRandomBall();
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        //setting array index number to be random range between 0-2
+        int ballNum = Random.Range(0, ballPrefabs.Length);
+        //changed the prefab array index to ballNum to insert random color
+        Instantiate(ballPrefabs[ballNum], spawnPos, ballPrefabs[ballNum].transform.rotation);
+        //time will range between 2 and 4 seconds 
+        Invoke("SpawnRandomBall", Random.Range(2f, 4f));
+    }
+
+}
